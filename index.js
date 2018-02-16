0a4169ff2bd5d72925b3b41cfe4f71a243065756
Rik Git <r.git@lenta.ru>
2018-02-16 12:22:06 +0300

    '' ->

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 "use strict";
 
-let ogrnNumber;
-
 function isOgrn(ogrnNumber){
-  console.log('Number = ' + ogrnNumber);
-  let ogrnValue = String(ogrnNumber);
-  let ogrnValueLength = ogrnValue.length;
+  console.log(`Number = ${ogrnNumber}`);
+  const ogrnValue = String(ogrnNumber);
+  const ogrnValueLength = ogrnValue.length;
 
   if (checkValidate(ogrnValue,ogrnValueLength)) {
     let ogrnDivisorValue = getDivisor(ogrnValueLength);
-    let ogrnValueLast = ogrnValue.slice(-1);
-    let remainderDivision = ogrnValue.slice(0, -1) % ogrnDivisorValue;
+    const ogrnValueLast = ogrnValue.slice(-1);
+    const remainderDivision = ogrnValue.slice(0, -1) % ogrnDivisorValue;
     if (remainderDivision == ogrnValueLast || remainderDivision == 10 && ogrnValueLast == 0) {
-      console.log('Ccorrect number.' + showOgrnType(ogrnValueLength));
+      console.log(`Ccorrect number. ${showOgrnType(ogrnValueLength)}`);
     }
-    else console.log('Number not ogrn.');
+    else console.log(`Number not ogrn.`);
   }
 }
 
@@ -26,12 +24,12 @@ function checkValidate(a,b) {
 }
 
 function showErrorMessage(){
- console.log('Iincorrect value.');
+ console.log(`Iincorrect value.`);
  return;
 }
 
 function showOgrnType(c){
- return 'This is ' + (c == 13 ? 'OGRNIP' : 'OGRNUL') + '.';
+ return `This is ${(c == 13 ? 'OGRNIP' : 'OGRNUL')}.`;
 }
 
 function getDivisor(i) {
